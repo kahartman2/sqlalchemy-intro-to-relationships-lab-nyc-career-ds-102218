9230b891a9cd7e498c5cafd2c8d0cb5d56c29426
Kayla Hartman <kahartman95@gmail.com>
2018-11-05 16:22:21 -0500

    our solutions

--- queries.py
+++ queries.py
@@ -6,9 +6,15 @@ engine = create_engine('sqlite:///actors.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
 def return_gwyneth_paltrows_roles():
-    pass
+    actor_object = session.query(Actor).filter(Actor.name == 'Gwyneth Paltrow')
+    for x in actor_object:
+        new_list = x.roles
+    return list(map(lambda x: x.character, new_list))
 
 def return_tom_hanks_2nd_role():
-    pass
+    actor_object = session.query(Actor).filter(Actor.name == 'Tom Hanks')
+    for x in actor_object:
+        new_list = x.roles
+    new_list = list(map(lambda x: x.character, new_list))
+    return new_list[1]
--- seed.py
+++ seed.py
@@ -7,3 +7,20 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # add and commit the actors and roles below
+
+tom_hanks = Actor(name = 'Tom Hanks')
+gwyneth_paltrow = Actor(name = 'Gwyneth Paltrow')
+jennifer_anniston = Actor(name = 'Jennifer Anniston')
+
+forest_gump = Role(character = 'Forest Gump', actors = tom_hanks)
+jim_lovell = Role(character = 'Jim Lovell', actors = tom_hanks)
+woody = Role(character = 'Woody', actors = tom_hanks)
+robert_langdon = Role(character = 'Robert Langdon', actors = tom_hanks)
+
+pepper_potts = Role(character = 'Pepper Potts', actors = gwyneth_paltrow)
+margot_tenenbaum = Role(character = 'Margot Tenenbaum', actors = gwyneth_paltrow)
+
+rachel_green = Role(character = 'Rachel Green')
+
+session.add_all([tom_hanks, gwyneth_paltrow, jennifer_anniston, forest_gump, jim_lovell, woody, robert_langdon, pepper_potts, margot_tenenbaum, rachel_green])
+session.commit()
